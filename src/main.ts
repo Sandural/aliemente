dc0830a30db4c395e1410cab4cca19124a6f6cd1
mningchen <1370365840@qq.com>
2022-08-23 10:18:25 +0800

    feat: 添加测试

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,11 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+const app = createApp(App);
 
-createApp(App).mount('#app')
+app.config.globalProperties.$CATELINA = {
+  size: 'large'
+}
+
+
+app.mount('#app')
